34a2d61a27cc6cfeee5ef19adf1c83a268c71eb9
Keith Thompson <keithm_thompson@outlook.com>
2017-04-07 22:19:09 -0400

    adjust webpack confconfig file because ReactJS is not used

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,13 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015']
         }
       }
     ]
   },
   devtool: 'source-maps',
   resolve: {
-    extensions: ["", ".js", ".jsx" ]
+    extensions: ["", ".js"]
   }
 };
